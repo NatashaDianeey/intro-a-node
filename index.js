5533de5a77252425825ca03131bdb7eefb075156
NatashaDianeey <ndianeey@gmail.com>
2019-02-07 21:31:48 -0600

    Seccond commit -work with cowsay

---
+++ index.js
@@ -0,0 +1,13 @@
+var cowsay = require("cowsay");
+
+console.log(cowsay.say({
+	text : "Soy una vaquita buena onda",
+	e : "++",
+    T : "V"
+}));
+
+// console.log('hola');
+
+// console.log('estoy abriendo archivo js en terminal');
+
+// or cowsay.think()
